89fa284f408d3733e90cb13569e5aebc7db4c19f
t <maurya111>
2022-02-22 19:32:07 +0530

    Changes done to run the code using postgres

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogsite',
+        'USER': 'triveni',
+        'PASSWORD': 'tri123VENI!@#',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
